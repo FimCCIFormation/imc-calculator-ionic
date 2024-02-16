b14d02bde4fd3e6ee3c479d8a50c793f6c0cf0e8
MOTTIN Benoit <BMOTTIN@fim-online.net>
2024-02-16 11:02:25 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'imcCalculator',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
